4961a681705fde1d457b629a2dc688dc28d2d862
cents97 <j.centsinno>
2025-03-10 23:17:19 +0300

    'all'

--- charity_website/settings.py
+++ charity_website/settings.py
@@ -49,6 +49,7 @@ SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware", 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -161,4 +162,6 @@ JAZZMIN_SETTINGS = {
     
     "site_brand": "St.Thaddeus Children's Foundation",
 }
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
